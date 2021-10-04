40f6663c2832fb77a119ae9cdac734a211fb956b
Jisan-mia <jisanmia47@gmail.com>
2021-10-04 16:08:45 +0600

    middlewares

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
+
 const router = require('express').Router()
+
+const apiKeyMiddleware = require('../middlewares/apiKey')
+
+// apply this middleware to all the route of this file
+// router.use(apiKeyMiddleware)
+
 
 router.get('/', (req, res) => {
   res.render('index', {
@@ -15,6 +22,20 @@ router.get('/about', (req, res) => {
 
 router.get('/download', (req, res) => {
   res.download(path.resolve(__dirname) + '/about.html')
+})
+
+// apply middleware to particular route
+router.get('/api/products', apiKeyMiddleware, (req, res) => {
+  res.json([
+    {
+      id: '123',
+      name: 'Samsung s6'
+    },
+    {
+      id: '124',
+      name: 'iPhone 11'
+    },
+  ])
 })
 
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express') // returns a funciton
 const path = require('path')
+const apiKeyMiddleware = require('./middlewares/apiKey')
 
 const app = express() 
 
@@ -16,12 +17,16 @@ app.set('view engine', 'ejs')
 
 
 
-// static middleware
+// static middleware (built-in for express)
 app.use(express.static('public'))
 
 
+// global middleware which apply to all
+// app.use(apiKeyMiddleware)
+
+
 app.use(mainRouter)
-// prefix
+// every router prefix
 // app.use('/en', mainRouter)
 
 // create server
@@ -32,23 +37,26 @@ app.listen(PORT, () => {
 
 
 
+
+/*
 // create home route 
-/* app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index', {
     title: 'Home Page'
   })
 })
- */
 
 // create about route
-/* app.get('/about', (req, res) => {
+app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Page'
   })
 })
- */
+
 
 // create download route which will download some file
-/* app.get('/download', (req, res) => {
+app.get('/download', (req, res) => {
   res.download(path.resolve(__dirname) + '/about.html')
-}) */
+}) 
+
+*/
